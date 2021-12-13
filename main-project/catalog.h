f4330c7187c11484adc0ccbce2ea3270d8e194d6
unknown <pollimolli210@gmail.com>
2021-12-13 22:50:49 +0300

    Добавлены структуры для хранения данных

---
+++ main-project/catalog.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef CATALOG_H
+#define CATALOG_H
+
+#include "constants.h"
+
+
+struct catalog
+{
+    unsigned double cost;
+    unsigned int quan;
+    char category;
+    char name[MAX_STRING_SIZE];
+};
+
+#endif
