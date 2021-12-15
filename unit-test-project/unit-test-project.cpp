cc144af3026f19663bea07bb8c68e9234860bc98
unknown <pollimolli210@gmail.com>
2021-12-16 02:03:31 +0300

    Добавлены тесты

--- unit-test-project/unit-test-project.cpp
+++ unit-test-project/unit-test-project.cpp
@@ -1,17 +1,62 @@
 ﻿#include "pch.h"
 #include "CppUnitTest.h"
-
+#include "../main-project/catalog.h"
+#include "../main-project/processing.h"
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace unittestproject
 {
+
+	catalog* build_subscription(double cost1, unsigned int quan1, char category1[MAX_STRING_SIZE])
+	{
+		catalog* prod = new catalog;
+		prod->cost = cost1;
+		prod->quan = quan1;
+		prod->category[MAX_STRING_SIZE] = category1[MAX_STRING_SIZE];
+		return prod;
+	}
+
+	//       
+	void delete_subscription(catalog* array[], int size)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			delete array[i];
+		}
+	}
+
+
+
 	TEST_CLASS(unittestproject)
 	{
 	public:
-		
+
 		TEST_METHOD(TestMethod1)
 		{
-			Assert::Fail();
+			catalog* prod[3];
+			prod[0] == build_subscription(10, 2, "Промтовары");
+			prod[1] == build_subscription(11, 1, "Продукты");
+			prod[2] == build_subscription(5, 4, "Промтовары");
+			Assert::AreEqual(40.0, process(prod, 3, "Промтовары"));
+			delete_subscription(prod, 3);
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			catalog* prod[3];
+			prod[0] == build_subscription(112, 2, "Промтовары");
+			prod[1] == build_subscription(11, 1, "Продукты");
+			prod[2] == build_subscription(7, 4, "Промтовары");
+			Assert::AreEqual(40.0, process(prod, 3, "Продукты"));
+			delete_subscription(prod, 3);
+		}
+		TEST_METHOD(TestMethod3)
+		{
+			catalog* prod[3];
+			prod[0] == build_subscription(15, 6, "Сладости");
+			prod[1] == build_subscription(31, 1, "Сладости");
+			prod[2] == build_subscription(7, 4, "Сладости");
+			Assert::AreEqual(128.0, process(prod, 3, "Сладости"));
+			delete_subscription(prod, 3);
 		}
 	};
 }
